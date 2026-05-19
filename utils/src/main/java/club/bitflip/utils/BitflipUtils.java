40a89d51e019ee24ca87cfacf8f0be48bf99ea92
UnheardWolf344 <griffin3868@gmail.com>
2026-05-18 22:22:47 -0400

    Figuring out maven repository

---
+++ utils/src/main/java/club/bitflip/utils/BitflipUtils.java
@@ -0,0 +1,10 @@
+package club.bitflip.utils;
+
+/**
+ * Main utility class for Bitflip-Utils.
+ */
+public class BitflipUtils {
+    public static String getVersion() {
+        return "1.0.0";
+    }
+}
